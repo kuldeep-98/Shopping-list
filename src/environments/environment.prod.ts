3c26b563bfc59629e00a7da1e76f2dc206d53bdc
Kuldeep <kuldeepbhama4455@gmail.com>
2020-01-21 00:04:10 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzTyVKk1hIHzNWLsU-Eb-nuwBjGQUXitU",
+    authDomain: "shopping-list-4b1f8.firebaseapp.com",
+    databaseURL: "https://shopping-list-4b1f8.firebaseio.com",
+    projectId: "shopping-list-4b1f8",
+    storageBucket: "shopping-list-4b1f8.appspot.com",
+    messagingSenderId: "243026055258",
+    appId: "1:243026055258:web:64c42a132b09c080678863",
+    measurementId: "G-SXCXNB9S64"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAzTyVKk1hIHzNWLsU-Eb-nuwBjGQUXitU",
+    authDomain: "shopping-list-4b1f8.firebaseapp.com",
+    databaseURL: "https://shopping-list-4b1f8.firebaseio.com",
+    projectId: "shopping-list-4b1f8",
+    storageBucket: "shopping-list-4b1f8.appspot.com",
+    messagingSenderId: "243026055258",
+    appId: "1:243026055258:web:64c42a132b09c080678863",
+    measurementId: "G-SXCXNB9S64"
+  }
 };
 
 /*
